717a99d953882d4119cde5dc46287e5d26711a80
MattV <mvpetersen1@gmail.com>
2021-03-22 21:37:17 -0600

    updated get route for landing page html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,7 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
+const db = require("../models");
+const api = require("./api-routes");
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -8,7 +10,7 @@ module.exports = function(app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/velocity");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
@@ -16,14 +18,27 @@ module.exports = function(app) {
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/velocity");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
+//   app.get("/members", isAuthenticated, (req, res) => {
+//     res.sendFile(path.join(__dirname, "../public/members.html"));
+//   });
+  app.get("/velocity", isAuthenticated, (req, res) => {
+      
+      db.Task.findAll({}).then((dbtask) => {
+          res.json(dbtask)
+          console.log(dbtask);
+          let dbtaskObject = {
+              task: dbtask
+          }
+          console.log(dbtaskObject);
+          res.render('index', dbtaskObject)
+        });
+
+  })
 };
