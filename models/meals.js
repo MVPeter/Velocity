e91882e308d16f667da804b9f01192ed084b2c4c
spencercreer <spencercreer@gmail.com>
2021-03-24 18:12:35 -0600

    added Foods.csv to db, added html to handlebars views

---
+++ models/meals.js
@@ -0,0 +1,22 @@
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Meal extends Model {
+
+    };
+    Meal.init({
+        meal: DataTypes.STRING,
+        description: DataTypes.STRING,
+        serving_size: DataTypes.STRING,
+        // THESE WILL NEED TO BE DECIMAL
+        calories: DataTypes.DECIMAL(5,2),
+        carbs: DataTypes.DECIMAL(5,2),
+        fat: DataTypes.DECIMAL(5,2),
+        sodium: DataTypes.DECIMAL(5,2),
+    },{
+        sequelize,
+        modelName: 'Meal',
+    });
+    return Meal
+}
