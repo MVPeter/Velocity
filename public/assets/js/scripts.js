321fa43560c24a0a6bf8feb37bef411369e5b245
spencercreer <spencercreer@gmail.com>
2021-03-25 19:06:35 -0600

    added task text area, day, input checkbox, and delete icon.

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,12 +1,30 @@
 let addTaskButton = document.getElementById("addTaskBtn");
-let taskInput = document.getElementById("taskInput");
+let taskNameEl = document.getElementById("task-name");
+let taskTextEl = document.getElementById("task-text");
 
 function submitTask(){
-    let newTask = taskInput.value;
+    // This will need to be changed to a post to store the task to the db
+    let newTaskName = taskNameEl.value;
+    let newTaskText = taskTextEl.value;
+    // Create new list element, input checkbox, and delete button
     let newTaskRow = document.createElement("li");
-
-    newTaskRow.textContent = newTask; 
-    
+    newTaskRow.textContent = newTaskName; 
+    let newTaskInput = document.createElement("input");
+    newTaskInput.setAttribute("type", "checkbox");
+    let delBtn = document.createElement('i');
+    delBtn.classList.add(
+      'fas',
+      'fa-trash-alt',
+      'float-right',
+      'text-danger',
+      'delete-task'
+    );
+    newTaskRow.prepend(newTaskInput);
+    newTaskRow.append(delBtn);
     document.getElementById("tasks").append(newTaskRow);
+
+    taskNameEl.value = "";
+    taskTextEl.value = "";
+
 }
 addTaskButton.onclick = submitTask; 
