ac338c3fea9ad247e9fc045657f4b98690a1247e
MattV <mvpetersen1@gmail.com>
2021-03-27 16:20:10 -0600

    updated handlebars and partial

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,7 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 const db = require("../models");
+const Handlebars = require('handlebars');
 // const api = require("./api-routes");
 
 // Requiring our custom middleware for checking if a user is logged in
@@ -30,18 +31,29 @@ module.exports = function(app) {
 //     res.sendFile(path.join(__dirname, "../public/members.html"));
 //   });
   app.get("/landing", isAuthenticated, async (req, res) => {
+    
+ 
+
+
+    try{
+        await db.Task.findAll().then((task) => {
+            // res.json(dbtask)
+            console.log(task);
+            let dbtaskObject = {
+                task: task
+            }
+          
+            // console.log("routes object " + JSON.stringify(dbtaskObject.task[0]));
+            // console.log("routes object " + JSON.stringify(dbtask[0]))
+            res.render('index', {
+              tasks: task
+            });
+          });
+      }catch (error){
+        console.log(error)
+      }
       
-      // db.Task.findAll({}).then((dbtask) => {
-      //     // res.json(dbtask)
-      //     // console.log(dbtask);
-      //     let dbtaskObject = {
-      //         task: dbtask
-      //     }
-      //     console.log("routes object " + dbtaskObject);
-      //   });
-      let dbtaskObject = await db.Task.findAll({});
-      console.log(dbtaskObject);
+      
         
-      res.render('index', dbtaskObject);
   })
 };
