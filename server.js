b103eac24ac6fdcadea7856f65802a58108eefbf
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-03-30 22:50:37 -0600

    updates / readme

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ require("./routes/api-routes.js")(app);
 
 
 // Start server {force: true}
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force:true}).then(function () {
   app.listen(PORT, () =>
     console.log(`Server listening on: http://localhost:${PORT}`)
   );
